2a6eb03251ff5212282485564f9c69e51b558063
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-03 23:17:54 -0700

    first python code

---
+++ 01_band_name_generator/main.py
@@ -0,0 +1,10 @@
+print ("welcome to band name generator")
+# first_part_of_band_name = input ("what is your country name? ")
+# second_part_of_band_name = input ("what is your pet name? ")
+#
+# band_name = first_part_of_band_name + second_part_of_band_name
+
+
+
+#the code in one line
+print("your band name is: " + input ("what is your country name? ") + input ("what is your pet name? "))
