784a34311760fbb1efe8554371078128d69e5cd3
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-11 22:56:18 -0700

    add some improvements

--- 02_Tip_Calculator/main.py
+++ 02_Tip_Calculator/main.py
@@ -1,5 +1,5 @@
-total_bill = int(input ("What was the total bill? "))
-tip_percent = int(input ("How much tip would you like to give? 10, 12 or 15? "))
+total_bill = int(input ("What was the total bill? $"))
+tip_percent = int(input ("How much tip would you like to give? 10%, 12% or 15%? "))
 number_of_people = int(input ("How many people to split the bill? "))
 tip = round(((total_bill/number_of_people) * (tip_percent/100)), 2)
 
