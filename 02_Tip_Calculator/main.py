71d48b57aab033ea0b10bb38280cb294cfa6dce7
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-11 23:29:27 -0700

    add some improvements

--- 02_Tip_Calculator/main.py
+++ 02_Tip_Calculator/main.py
@@ -3,9 +3,10 @@ total_bill = int(input ("What was the total bill? $"))
 tip_percent = int(input ("How percentage tip would you like to give? 10, 12 or 15? (Please enter only the number without including the percentage sign.) "))
 number_of_people = int(input ("How many people to split the bill? "))
 tip = round(((total_bill/number_of_people) * (tip_percent/100)), 2)
+total_after_tip = total_bill + (tip * number_of_people)
 
-
-print(f"Each person should pay ${tip} as a tip")
+print(f"Each person should pay ${tip} as a tip, total payment will be ${(tip + (total_bill/number_of_people))}")
+print(f"Total bill after adding the tips is ${total_after_tip}")
 
 
 
