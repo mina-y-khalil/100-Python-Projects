3bd450468f249d62514fe98136fbe48631d532e3
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-13 21:56:45 -0700

    adding having a pic feature

--- 03_Roller_Coaster_Game/main.py
+++ 03_Roller_Coaster_Game/main.py
@@ -1,10 +1,28 @@
 print ("welcome to the Roller Coaster ")
 height = int(input("what is the height? "))
+# price list
+bill = 0
+adult_tickets = 25
+youth_tickets = 20
+child_tickets = 10
+picture_price = 3
+
 # nested if
 if height >= 120:
-    if (int(input("what is your age? "))) >= 18:
-        print("you have to pay full ticket this will cost $20")
+    if (int(input("what is your age? "))) <= 12:
+        bill = child_tickets
+        print(f"Child tickets are ${bill}")
+    elif height <= 18:
+        bill = youth_tickets
+        print(f"Youth tickets are ${bill}")
     else:
-        print("Good news! you will pay only half ticket $10")
+        bill = adult_tickets
+        print(f"Adult tickets are ${bill}")
+    wants_photo = input(f"Do you want to have a photo take for extra ${picture_price}? Type y for yes and n for No. ")
+    if wants_photo == "y":
+        print(f"Your total bill now is ${bill+picture_price}")
+    else:
+        print(f"Ok, your total bill is ${bill}")
+
 else:
     print("sorry! you are too short to play the game it is not safe for you")
