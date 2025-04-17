5c5afdab8197fbff4ebd86041b61595f70d489b4
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-17 15:26:52 -0700

    make correction

--- 04_Rock_Paper_Scissors/rock_paper_scissors.py
+++ 04_Rock_Paper_Scissors/rock_paper_scissors.py
@@ -51,5 +51,5 @@ elif computer_choice > user_choice:
     print("You lose!  🙃")
 elif user_choice > computer_choice:
     print("You win! 🥳")
-elif computer_choice == user_choice: #I didn’t use “else” here because it will never be executed.
+elif computer_choice == user_choice:
     print("It's a draw!")
