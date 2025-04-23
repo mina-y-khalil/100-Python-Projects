64ef9dbbe2cbe70d3ed708726235a5679f8a60c1
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-23 16:28:35 -0700

    Trying to add a placeholder that will be replaced with the correct letter

--- 07-Hangman_game/hangman_game.py
+++ 07-Hangman_game/hangman_game.py
@@ -1,10 +1,37 @@
-secret_word = "python"
-balance = 6
+#ranomly choose a word from the animals list
+import random
+print('''Hi there, let's play the Hangman game! If you're not familiar with the rules, you can visit the following link to learn how to play: "https://en.wikipedia.org/wiki/Hangman_(game)".\n
+We'll be guessing the name of an animal. You need to guess one letter from the animal's name. Let's get started!''')
+animals_list = [
+    "cat", "dog", "cow", "pig", "fox",
+    "bat", "ant", "bee", "dolphin", "rat",
+    "owl", "duck", "goat", "frog", "fish",
+    "bear", "deer", "lion", "elephant", "crab"
+]
+secret_word = random.choice(animals_list)
+print(secret_word)
+placeholder = ""
+
+for position in range (0,len(secret_word)):
+    placeholder += "_"
+# print(placeholder)
+
+main_score = 10
+balance = main_score
+display = ""
+
 while balance != 0:
-    user_input = str(input("what is your guess? ")).lower()
-    if user_input in secret_word:
-        print ("You have selected a correct letter 🤩")
-    elif user_input not in secret_word:
-        balance -=1
-        print(f"you lost one of your life your remaining score is {balance}/6")
+    user_input = str(input(f"what is your guess? ")).lower()
+    if user_input not in secret_word:
+        display += "_"
+        balance -= 1
+        print(f"you lost one of your life 😭 your remaining score is {balance}/{main_score} {display}")
+
+    else:
+        for letter in secret_word:
+            if letter == user_input:
+                display += letter
+        print(f"You have selected a correct letter 🤩 {display}")
+
+
 print("GAME OVER 🥲")
