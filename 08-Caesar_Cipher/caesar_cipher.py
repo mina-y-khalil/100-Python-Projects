9b37e0ddf4237eec14da4c2fae3883e62af71f84
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-27 22:49:15 -0700

    Make Caesar cipher code cleaner and simpler

--- 08-Caesar_Cipher/caesar_cipher.py
+++ 08-Caesar_Cipher/caesar_cipher.py
@@ -4,31 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text,shift_amount ):
+def encrypt(original_text,shift_amount, encode_or_decode ):
     the_new_message = ""
-    if direction == "encode":
-        for letter in original_text:
-            if letter in alphabet:
-                the_original_index = alphabet.index(letter)
-                the_new_index = (the_original_index + shift_amount) % len(alphabet)
-                letter = alphabet[the_new_index]
-                the_new_message += letter
-            else:
-                the_new_message += letter
-        print(f"Here is the encoded result: {the_new_message}")
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
+    for letter in original_text:
+        if letter in alphabet:
+            the_original_index = alphabet.index(letter)
+            the_new_index = (the_original_index + shift_amount) % len(alphabet)
+            letter = alphabet[the_new_index]
+            the_new_message += letter
+        else:
+            the_new_message += letter
+    print(f"Here is the encoded result: {the_new_message}")
 
-    elif direction == "decode":
-        for letter in original_text:
-            if letter in alphabet:
-                the_original_index = alphabet.index(letter)
-                the_new_index = (the_original_index - shift_amount) % len(alphabet)
-                letter = alphabet[the_new_index]
-                the_new_message += letter
-            else:
-                the_new_message += letter
-        print(f"Here is the decoded result: {the_new_message}")
-    else:
-        print("Please enter a valid value")
-
-encrypt(original_text=text,shift_amount=shift)
+encrypt(original_text=text,shift_amount=shift, encode_or_decode=direction)
