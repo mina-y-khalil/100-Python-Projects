666d04d6571d5f66ea5023e72103566e28d259e2
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-27 20:58:51 -0700

    add the encoding function

---
+++ 08-Caesar_Cipher/caesar_cipher.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text,shift_amount ):
+    the_new_message = ""
+    if direction == "encode":
+        for letter in original_text:
+            if letter in alphabet:
+                the_original_index = alphabet.index(letter)
+                the_new_index = the_original_index + shift_amount
+                letter = alphabet[the_new_index]
+                the_new_message += letter
+            else:
+                the_new_message += letter
+        print(the_new_message)
+
+
+    # elif direction == "decode":
+
+    else:
+        print("Please enter a valid value")
+
+encrypt(text,shift)
