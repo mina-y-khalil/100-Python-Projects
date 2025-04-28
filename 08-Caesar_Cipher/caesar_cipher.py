ae21c260ab82210132e7d6d569f8e89e17f85b36
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-04-27 21:58:41 -0700

    Add encode and decode functionality with proper shift handling

--- 08-Caesar_Cipher/caesar_cipher.py
+++ 08-Caesar_Cipher/caesar_cipher.py
@@ -10,7 +10,7 @@ def encrypt(original_text,shift_amount ):
         for letter in original_text:
             if letter in alphabet:
                 the_original_index = alphabet.index(letter)
-                the_new_index = (the_original_index + shift_amount) % 26
+                the_new_index = (the_original_index + shift_amount) % len(alphabet)
                 letter = alphabet[the_new_index]
                 the_new_message += letter
             else:
@@ -22,7 +22,7 @@ def encrypt(original_text,shift_amount ):
         for letter in original_text:
             if letter in alphabet:
                 the_original_index = alphabet.index(letter)
-                the_new_index = (the_original_index - shift_amount) % 26
+                the_new_index = (the_original_index - shift_amount) % len(alphabet)
                 letter = alphabet[the_new_index]
                 the_new_message += letter
             else:
