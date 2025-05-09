8cdf41ca973e13fd60defb37ab39fc72693c0104
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-05-08 22:33:14 -0700

    implement secret auction with highest bidder logic

--- 09_Blind_Auction/blind_auction.py
+++ 09_Blind_Auction/blind_auction.py
@@ -3,12 +3,30 @@ from art import logo
 print(logo)
 print("Welcome to the secret auction program. ")
 
-still_bidders = True
+continue_bidding = True
 bids = {}
 
-while still_bidders :
-    user_name = input("What is your name? ")
-    user_bid = int(input("What's your bide? $"))
-    still_bidders = input("Are there any other bidders? Type 'yes' or 'no' ").lower()
-    if still_bidders == "no":
-        still_bidders = False
+while continue_bidding :
+    #Ask the user for input
+    user_name = input("What is your name?: ")
+    user_bid = int(input("What's your bid? $"))
+
+    #save the input data to dictionary
+    bids[user_name] = user_bid
+
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no' ").lower()
+    if should_continue == "no":
+        continue_bidding = False
+        def find_highest_bidder(bidding_dictionary):
+            winner = ""
+            highest_bid = 0
+            for bidder in bidding_dictionary:
+                bid_amount = bidding_dictionary[bidder]
+                if bid_amount > highest_bid:
+                    highest_bid = bid_amount
+                    winner = bidder
+            print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+        print(find_highest_bidder(bids§))
+
+
