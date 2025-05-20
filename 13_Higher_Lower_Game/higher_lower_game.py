ae912ecd65074cb3f0ae0c0bc7681ae8438c286d
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-05-20 14:46:03 -0700

    bugfix: ensure each round runs with unique A and B

--- 13_Higher_Lower_Game/higher_lower_game.py
+++ 13_Higher_Lower_Game/higher_lower_game.py
@@ -26,24 +26,27 @@ score = 0
 
 while still_correct:
     b_choice = random.choice(data)
-    if a_choice == b_choice:
+    while a_choice == b_choice:
         b_choice = random.choice(data)
-        print(f"Compare A: {format_data(a_choice)} ")
-        print(vs)
-        print(f"Against B: {format_data(b_choice)}")
-        user_choice = (input("Who has the more followers? Type 'A' or 'B' ").lower())
 
-        #check if the user is correct.
-        a_follower_count = a_choice["follower_count"]
-        b_follower_count = b_choice["follower_count"]
-        is_correct(user_choice,a_follower_count,b_follower_count)
-        if is_correct(user_choice,a_follower_count,b_follower_count):
-            a_choice = b_choice
-            score += 1
-            print(f"You're right! Current score {score}")
-        else:
-            print(f"Sorry, that's wrong. Final score: {score}")
-            still_correct = False
+    print(f"Compare A: {format_data(a_choice)} ")
+    print(vs)
+    print(f"Against B: {format_data(b_choice)}")
+    user_choice = (input("Who has the more followers? Type 'A' or 'B' ").lower())
+
+    # check if the user is correct.
+    a_follower_count = a_choice["follower_count"]
+    b_follower_count = b_choice["follower_count"]
+    is_correct(user_choice, a_follower_count, b_follower_count)
+    if is_correct(user_choice, a_follower_count, b_follower_count):
+        a_choice = b_choice
+        score += 1
+        print(f"You're right! Current score {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        still_correct = False
+
+
 
 
 
