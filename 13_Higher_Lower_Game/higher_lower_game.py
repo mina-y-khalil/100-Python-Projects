cb047dcc33e4acdc3ef010e882d6e874ab988e76
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-05-20 14:42:00 -0700

    bugfix: ensure each round runs with unique A and B

--- 13_Higher_Lower_Game/higher_lower_game.py
+++ 13_Higher_Lower_Game/higher_lower_game.py
@@ -12,7 +12,7 @@ def format_data(account):
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
 
-def if_correct(user_guess, a_followers, b_followers):
+def is_correct(user_guess, a_followers, b_followers):
     """Take a user's guess and the follower counts and returns if they got it right."""
     if a_followers > b_followers:
        return user_guess == "a"
@@ -20,14 +20,14 @@ def if_correct(user_guess, a_followers, b_followers):
         return user_guess == "b"
 
 a_choice = random.choice(data)
-b_choice = random.choice(data)
-if a_choice == b_choice:
-    b_choice = random.choice(data)
 
-points = 0
+score = 0
 
 
 while still_correct:
+    b_choice = random.choice(data)
+    if a_choice == b_choice:
+        b_choice = random.choice(data)
         print(f"Compare A: {format_data(a_choice)} ")
         print(vs)
         print(f"Against B: {format_data(b_choice)}")
@@ -36,7 +36,14 @@ while still_correct:
         #check if the user is correct.
         a_follower_count = a_choice["follower_count"]
         b_follower_count = b_choice["follower_count"]
-
+        is_correct(user_choice,a_follower_count,b_follower_count)
+        if is_correct(user_choice,a_follower_count,b_follower_count):
+            a_choice = b_choice
+            score += 1
+            print(f"You're right! Current score {score}")
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            still_correct = False
 
 
 
