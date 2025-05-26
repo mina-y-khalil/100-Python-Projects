04017a928368a0d3074834da58e41d379b54d32c
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-05-25 21:19:04 -0700

    feat: add question loop control to QuizBrain with still_has_questions method

--- 16_Quiz_Game/quiz_brain.py
+++ 16_Quiz_Game/quiz_brain.py
@@ -3,6 +3,13 @@ class QuizBrain:
         self.q_number = 0
         self.q_list = q_list
 
+    def still_has_questions(self):
+        return self.q_number < len(self.q_list) #this will return True or False to manage the while loop in main.py
+
     def next_question(self):
         current_question = self.q_list[self.q_number]
+        self.q_number += 1
         input(f"Q{self.q_number}: {current_question.text} (True/False): ")
+
+
+
