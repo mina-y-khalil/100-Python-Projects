73ed40f7131e4257cab655297ff234db0922dac3
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-05-30 22:26:00 -0700

    Add turtle movement controls with WASD keys and update README

--- 18_Etch_A_Sketch/main.py
+++ 18_Etch_A_Sketch/main.py
@@ -24,9 +24,9 @@ screen = Screen()
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards) # Passing function as argument (callback)
-screen.onkey(key="s", fun=move_backwards) # Passing function as argument (callback)
-screen.onkey(key="a", fun=turn_left) # Passing function as argument (callback)
-screen.onkey(key="d", fun=turn_right) # Passing function as argument (callback)
-screen.onkey(key="c", fun=clear) # Passing function as argument (callback)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
 
 screen.exitonclick()
