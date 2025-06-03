5578c9be338af88c4d209f9c027e682b1354854f
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-02 20:49:11 -0700

    Initialize snake game structure with movement, food, and collision TODOs

---
+++ 20_Snake_Game/main.py
@@ -0,0 +1,7 @@
+#TODO 01: Create a snake body
+#TODO 02: Move the snake
+#TODO 03: Create snake food
+#TODO 04: Detect collision with food
+#TODO 05: Create a scoreboard
+#TODO 06: Detect collision with wall
+#TODO 07: Detect collision with tail
