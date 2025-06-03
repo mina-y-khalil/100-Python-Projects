829a38f30136b68c343519806c86f5d0d7ed75df
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-02 22:12:34 -0700

    Create initial snake body and basic movement loop (TODO 01–02)

--- 20_Snake_Game/main.py
+++ 20_Snake_Game/main.py
@@ -1,24 +1,33 @@
 #TODO 01: Create a snake body
 from turtle import Turtle, Screen
-
-#snake body
-tim = Turtle(shape="square")
-tim.penup()
-tim.color("white")
+import time
 
 #screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game by: Mina Y, Khalil")
+screen.tracer(0) # Stop auto-refresh
 
 
+#snake body
+starting_positions = [(0,0) , (-20,0), (-40,0)]
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(position)
+    segments.append(new_segment)
 
 #TODO 02: Move the snake
-def forward ():
-    tim.forward(10)
-screen.listen()
-screen.onkey(key="w", fun=forward)
+game_is_on = True
+while game_is_on:
+    screen.update()  # Refresh screen once
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
 
 
 #TODO 03: Create snake food
