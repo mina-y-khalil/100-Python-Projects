bea3a9ca1b40f589774af3cf6a71ab06deae722e
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-05 21:56:56 -0700

    Set up initial screen for Pong game (TODO 01)

---
+++ 21-The_Pong_Game/main.py
@@ -0,0 +1,21 @@
+#TODO 01: Create the screen
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("The Pong Game")
+
+#TODO 02: Create and move a paddle
+
+
+#TODO 03: Create another paddle
+#TODO 04: Create the ball and make it move
+#TODO 05: Detect Collision with wall and bounce
+#TODO 06: Detect when paddle misses
+#TODO 07: Keep Score
+
+
+
+
+screen.exitonclick()
