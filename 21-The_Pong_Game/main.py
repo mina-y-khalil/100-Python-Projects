3d50b409e299b28cebe7266bf92ef07ebd42b691
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-05 22:23:32 -0700

    Create screen and right paddle with basic up/down movement (TODO 01–02)

--- 21-The_Pong_Game/main.py
+++ 21-The_Pong_Game/main.py
@@ -1,4 +1,5 @@
 #TODO 01: Create the screen
+from ctypes.macholib.dyld import dyld_default_search
 from turtle import Screen, Turtle
 
 screen = Screen()
@@ -7,6 +8,25 @@ screen.setup(width=800,height=600)
 screen.title("The Pong Game")
 
 #TODO 02: Create and move a paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() +20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() -20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
 
 
 #TODO 03: Create another paddle
