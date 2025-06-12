a8e9c099a3bc33b7342342d46a472ec9cf5a13ee
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-11 22:44:26 -0700

    wip: practice code

--- 24_U.S._States_Game/main.py
+++ 24_U.S._States_Game/main.py
@@ -2,14 +2,20 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     # print(data)
+#     for row in data:
+#         # print(row)
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    # print(data)
-    for row in data:
-        # print(row)
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(data)
+print(data["temp"])
