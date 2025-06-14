8c78e8db7e687584d6c02febdf8ba8237c5d7969
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-13 22:08:36 -0700

    wip: practice code

--- 24_U.S._States_Game/main.py
+++ 24_U.S._States_Game/main.py
@@ -23,8 +23,19 @@ data = pandas.read_csv("weather_data.csv")
 data_dict = data.to_dict()
 # print(data_dict)
 temp_list = data["temp"].to_list()
-print(temp_list)
+# print(temp_list)
 average = round((sum(temp_list) / len(temp_list)), 2)
-print(average)
-print(data["temp"].mean()) #getting the average
-print(data["temp"].max())
+# print(average)
+# print(data["temp"].mean()) #getting the average
+# print(data["temp"].max())
+
+
+#get data in columns
+# print(data.condition)
+
+#get data in rows
+# print(data[data.day == "Monday"])
+
+max_temp = data["temp"].max()
+print(data[data.temp == max_temp])
+
