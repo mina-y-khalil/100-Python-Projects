eee12a1606ae4c147a0eb3d5a249f7104215720b
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-14 21:25:42 -0700

    Display guessed state name at correct map coordinates

--- 24_U.S._States_Game/main.py
+++ 24_U.S._States_Game/main.py
@@ -9,9 +9,18 @@ img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
 user_input = screen.textinput(title="Guess the State", prompt="What is your guess?")
+
+if user_input in all_states:
+    t = Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == user_input] #getting the row that matches the user input
+    t.goto(state_data.x.item , state_data.y.item )
+    t.write(state_data.state)
 
 
 
