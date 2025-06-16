6beb58487dfadf4bd72ff7f76b13d3fba83bcbbc
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-15 21:55:29 -0700

    Refactor missing states logic using list comprehension

--- 24_U.S._States_Game/main.py
+++ 24_U.S._States_Game/main.py
@@ -17,10 +17,10 @@ while len(guessed_states) < 50:
 
     user_input = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What is your guess?").title()
     if user_input == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         # print(missing_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
