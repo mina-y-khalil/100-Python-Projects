7726bf6d14c7a98a7205145327c2938ee3c69271
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-19 20:00:22 -0700

    Add basic Flask routes with dynamic greeting for training

--- 55_Higher_Or_Lower_URLs/main.py
+++ 55_Higher_Or_Lower_URLs/main.py
@@ -3,15 +3,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Different routes using the app.route decorator
 @app.route('/')
 def hello_world():
     return 'Hello, World'
 
-
+# creating a variable path and converting the path to a specific data type
 @app.route('/<name>/<int:number>')
 def greet(name, number):
     return f'hello there {name}, you are {number} years old!'
 
 
 if __name__ == '__main__':
+    #Run the app in debug mode to auto-reload
     app.run(debug=True)
