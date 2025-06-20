e6156a5618ca302c6d6fcc482f3caaabcb72495c
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-19 22:43:33 -0700

    feat: add number guessing game with dynamic routes and GIF feedback

--- 55_Higher_Or_Lower_URLs/README.md
+++ 55_Higher_Or_Lower_URLs/README.md
@@ -2,3 +2,5 @@ This project has not been completed yet.
 <p align="center">
   <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXVpZzU1YjVxeHpzYXh0NnNxNWNycHUzYWMxNHozNW1xNmlpZmhxciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11ASZtb7vdJagM/giphy.gif" alt="Loading..." />
 </p>
+
+
--- 55_Higher_Or_Lower_URLs/main.py
+++ 55_Higher_Or_Lower_URLs/main.py
@@ -1,17 +1,44 @@
 from flask import Flask
+import random
 
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper_function():
+        content = function()
+        return f'<b>{content}</b>'
+    return wrapper_function
+
+random_number = random.randint(0,9)
+# print(random_number)
+
+
 #Different routes using the app.route decorator
 @app.route('/')
-def hello_world():
-    return 'Hello, World'
+def home():
+    return ('<h1 style="text-align:center">Hello</h1>'
+            '<p style="text-align:center">Guess a number between 0 and 9.</p>'
+            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" ">')
+
 
 # creating a variable path and converting the path to a specific data type
-@app.route('/<name>/<int:number>')
-def greet(name, number):
-    return f'hello there {name}, you are {number} years old!'
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
+
+
+
 
 
 if __name__ == '__main__':
