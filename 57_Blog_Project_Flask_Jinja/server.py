ebde559a9761dca4ee19ca860232488286b2759f
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-22 22:26:37 -0700

    initialize Flask blog project with Jinja templates

--- 57_Blog_Project_Flask_Jinja/server.py
+++ 57_Blog_Project_Flask_Jinja/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 
 app = Flask(__name__)
@@ -6,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    return render_template("index.html", num=random_number)
 
 
 
