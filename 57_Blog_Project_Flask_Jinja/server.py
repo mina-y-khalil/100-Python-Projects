8626e92f76ec981a1ebafb41ab60b8eedb3e0833
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-23 16:21:50 -0700

    Display blog titles and subtitles using Jinja loop in index.html

--- 57_Blog_Project_Flask_Jinja/server.py
+++ 57_Blog_Project_Flask_Jinja/server.py
@@ -1,11 +1,17 @@
+import requests
 from flask import Flask, render_template
+import templates
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_requests = requests.get(blog_url)
+    all_blog = blog_requests.json()
+
+    return render_template("index.html",all_blog = all_blog)
 
 if __name__ == "__main__":
     app.run(debug=True)
