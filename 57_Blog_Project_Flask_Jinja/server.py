e810df3941dff9d5c147ef8d7d9ef996fc76c546
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-22 23:45:07 -0700

    Implement Flask routes for homepage and name-based age/gender guessing using APIs

--- 57_Blog_Project_Flask_Jinja/server.py
+++ 57_Blog_Project_Flask_Jinja/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 
 app = Flask(__name__)
@@ -14,7 +15,17 @@ def home():
 
 @app.route('/guess/<name>')
 def guess(name):
-    return render_template("guess.html", name=name)
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_date = gender_response.json()
+    gender = gender_date["gender"]
+
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+
+    return render_template("guess.html", name=name, gender=gender, age=age)
 
 
 
