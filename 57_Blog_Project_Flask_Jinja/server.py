19fb3b552cd6ee65136004ef76cd97677cc483f3
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-28 00:15:09 -0700

    Correct show_post loop to handle valid post matching properly

--- 57_Blog_Project_Flask_Jinja/server.py
+++ 57_Blog_Project_Flask_Jinja/server.py
@@ -1,17 +1,24 @@
 import requests
 from flask import Flask, render_template
-import templates
 
 
 app = Flask(__name__)
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_requests = requests.get(blog_url)
+all_blog = blog_requests.json()
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    blog_requests = requests.get(blog_url)
-    all_blog = blog_requests.json()
 
     return render_template("index.html",all_blog = all_blog)
+
+@app.route('/post/<int:index>')
+def show_post (index):
+    for post in all_blog:
+        if post['id'] == index:
+            return render_template("post.html", post=post)
+
+    return "Bad Data"
 
 if __name__ == "__main__":
     app.run(debug=True)
