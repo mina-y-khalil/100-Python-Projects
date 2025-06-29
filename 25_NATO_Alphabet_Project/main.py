c54352afbcd205058c8a565cc618e1e3c254cb2b
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-06-28 19:51:03 -0700

    feat: build NATO phonetic translator from user input using CSV data

--- 25_NATO_Alphabet_Project/main.py
+++ 25_NATO_Alphabet_Project/main.py
@@ -1,6 +1,6 @@
 student_dict = {
     "student": ["Mina", "Leo", "Laquisha", "Thomas"],
-    "score": [56, 76, 98]
+    "score": [56, 76, 98, 88]
 }
 
 #Looping through dictionaries:
@@ -19,9 +19,14 @@ for (index, row) in student_data_frame.iterrows():
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
 
